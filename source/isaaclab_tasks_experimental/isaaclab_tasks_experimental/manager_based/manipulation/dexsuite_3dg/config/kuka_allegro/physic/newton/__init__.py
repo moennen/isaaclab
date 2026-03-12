265c3ddcf90b1777d27ccb39d4860b813b39c3cc
Nicolas Moenne-Loccoz <nicolasm@nvidia.com>
2026-03-12 12:55:34 -0400

    Add newton manager extension for dexsuite-3dg tasks.

---
+++ source/isaaclab_tasks_experimental/isaaclab_tasks_experimental/manager_based/manipulation/dexsuite_3dg/config/kuka_allegro/physic/newton/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2022-2026, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Newton physics config and extended manager for Dexsuite 3dg."""
+
+from .dexsuite_3dg_newton_cfg import Dexsuite3dgNewtonCfg
+from .dexsuite_3dg_newton_manager import Dexsuite3dgNewtonManager
+
+__all__ = ["Dexsuite3dgNewtonCfg", "Dexsuite3dgNewtonManager"]
